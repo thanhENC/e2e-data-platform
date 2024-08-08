9d816270275a6f07597f8fee376dbabdf30bd55e
ThanhENC <thanhenc@gmail.com>
2024-08-08 18:37:07 +0000

    minio docker

---
+++ minio/start_dockerfile.sh
@@ -0,0 +1,1 @@
+docker run -p 9000:9000 -p 9001:9001 --name minio -v ~/minio/data:/data -e "MINIO_ROOT_USER=ROOTNAME" -e "MINIO_ROOT_PASSWORD=123456789" quay.io/minio/minio server /data --console-address ":9001" 
