d43556721fb19f2e9c3defdddc535e171b4e00fc
thanhENC <thanhenc@gmail.com>
2024-09-30 10:20:40 +0000

    fea: dbt fact sales

--- dbt/models/staging/adw/sales/stg_adw__salesorderdetail.sql
+++ dbt/models/staging/adw/sales/stg_adw__salesorderdetail.sql
@@ -1,18 +1,24 @@
 with source as (
-      select * from {{ source('source_adw_sales', 'sa_salesorderdetail') }}
+      select
+        *, 
+        CAST( {{ adapter.quote("unitprice") }} as DECIMAL(20, 3) ) as decimal_unit_price,
+        CAST( {{ adapter.quote("unitpricediscount") }} as DECIMAL(10, 3) ) as decimal_unit_price_discount
+      from {{ source('source_adw_sales', 'sa_salesorderdetail') }}
 ),
+
 renamed as (
     select
-        {{ adapter.quote("SALESORDERID") }} as sales_order_id,
-        {{ adapter.quote("SALESORDERDETAILID") }} as sales_order_detail_id,
-        {{ adapter.quote("CARRIERTRACKINGNUMBER") }} as carrier_tracking_number,
-        {{ adapter.quote("ORDERQTY") }} as order_quantity,
-        {{ adapter.quote("PRODUCTID") }} as product_id,
-        {{ adapter.quote("SPECIALOFFERID") }} as special_offer_id,
-        {{ adapter.quote("UNITPRICE") }} as unit_price,
-        {{ adapter.quote("UNITPRICEDISCOUNT") }} as unit_price_discount,
-        {{ adapter.quote("ROWGUID") }} as row_guid,
-        {{ adapter.quote("MODIFIEDDATE") }} as modified_date
+        {{ adapter.quote("salesorderid") }} as sales_order_id,
+        {{ adapter.quote("salesorderdetailid") }} as sales_order_detail_id,
+        {{ adapter.quote("carriertrackingnumber") }} as carrier_tracking_number,
+        {{ adapter.quote("orderqty") }} as order_quantity,
+        {{ adapter.quote("productid") }} as product_id,
+        {{ adapter.quote("specialofferid") }} as special_offer_id,
+        {{ adapter.quote("decimal_unit_price") }} as unit_price,
+        {{ adapter.quote("decimal_unit_price_discount") }} as unit_price_percent_discount,
+        CAST( decimal_unit_price * ( 1.0 - decimal_unit_price_discount ) * orderqty as DECIMAL(20, 3) ) as line_total,
+        {{ adapter.quote("rowguid") }} as row_guid,
+        {{ adapter.quote("modifieddate") }} as modified_date
 
     from source
 )
